f55d393c265b49bec8dcacaa01abcbf7685aeedd
gigi2804 <luigi.mastrocinque@mail.polimi.it>
2026-07-11 11:30:13 +0200

    icona home

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Un Service Worker basilare per abilitare l'installazione PWA
+self.addEventListener('fetch', function(event) {
+    // Lasciamo che il browser gestisca le richieste di rete normalmente
+});
